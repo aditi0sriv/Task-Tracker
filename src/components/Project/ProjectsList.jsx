041bf3c6af3331ee2824ab70eb6a9e33c42490dc
aditi0sriv <aditi0sriv@gmail.com>
2025-05-02 23:04:09 +0530

    ProjectsList name bug fix

--- src/components/Project/ProjectsList.jsx
+++ src/components/Project/ProjectsList.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../Project/Projectslist.css';
+import '../Project/ProjectsList.css';
 
 export default function ProjectsList() {
   const [projects, setProjects] = useState([]);
